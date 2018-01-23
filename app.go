6e0891c9da22007b520b8ee423ad7e6c3765e1aa
Sergio Muñoz <tito.9.24@gmail.com>
2018-01-24 00:16:34 +0100

    imports

--- app.go
+++ app.go
@@ -7,12 +7,14 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"fmt"
 	"log"
+	"ileave_server/model"
 )
 
 type App struct {
 	Router *mux.Router
 	DB     *sql.DB
 }
+
 
 func (a *App) Initialize(user, password, dbname string) {
 	connectionString := fmt.Sprintf("%s:%s@tcp(moonfishteam.com:24396)/%s", user, password, dbname)
@@ -22,6 +24,13 @@ func (a *App) Initialize(user, password, dbname string) {
 		log.Fatal(err)
 	}
 	a.Router = mux.NewRouter()
+
+	c := model.Content{}
+	
+}
+
+func (a *App) initializeRoutes() {
+	//a.Router.HandleFunc("/content", )
 }
 
 func (a *App) Run(addr string) { }
---
+++ model/content.go
@@ -0,0 +1,10 @@
+package model
+
+
+type Content struct {
+	ID    int    `json:"id"`
+	Title  string `json:"title"`
+	Author   string    `json:"author"`
+	Url   string    `json:"url"`
+	Name   string    `json:"name"`
+}
